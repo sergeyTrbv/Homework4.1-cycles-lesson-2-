2664b33600d7c216ea9f48ed46fad2031efb5005
1 <sergey.trbv@yandex.ru>
2022-09-16 19:08:17 +0400

    Переделал дз 1. Убрал лишнее в первой задаче + сделал счётчик по подсчёту месяцев. 2.В домашнем задании 1 , 3 таск, переменную Y сделал с маленькой буквы 3.В Домашнем задании 2 , таск 1,2 . Цель в 12000000 вынес в переменную. Переменную moneyWallet1 переименовал в month. В таск 3 создал переменную для подсчёта месяцев нужных дял накоплений. В Домашнем задании 3 создал переменные в 1 таск для 1822 года и 2122 года

--- src/Main.java
+++ src/Main.java
@@ -9,27 +9,15 @@ public class Main {
 
         System.out.println("\nHomework 1 \ntask 1");
 
-        int salary1 = 65535;
-        int bank = 0;
-        int n = 0;
-        for (; bank < 1_000_000; n++) {
-            bank = bank + (bank / 100);
-            bank = bank + salary1;
-            if (n % 5 == 0) {
-                System.out.println(bank);
-            }
-            // System.out.println("месяц " + n + " сумма накоплений равна " + bank);
-        }
-        //System.out.println(bank);
-        //System.out.println(i);
-
-
         int salary = 65535;
         int piggyBank = 0;
-        while (piggyBank < 2_459_000) {
+        int piggyBankComplited = 2_459_000;
+        int f = 0;
+        while (piggyBank <= piggyBankComplited) {
             piggyBank = piggyBank + piggyBank / 100;
             piggyBank = piggyBank + salary;
-            System.out.println(piggyBank + " ruble");
+            f++;
+            System.out.println("mounth " + f + " bill " + piggyBank + " ruble");
         }
 
         System.out.println("\nTask 2");
@@ -46,15 +34,15 @@ public class Main {
 
         System.out.println("\n\nTask 3");
 
-        int Y = 12_000_000;
+        int y = 12_000_000;
         double year = 0;
         double rojdaemost = (double) 17 / 1000;
         double smertnost = (double) 8 / 1000;
 
         while (year < 10) {
-            Y = (int) (Y + (Y * rojdaemost) - (Y * smertnost));
+            y = (int) (y + (y * rojdaemost) - (y * smertnost));
             year++;
-            System.out.println(Y);
+            System.out.println(y);
         }
     }
 
@@ -65,8 +53,9 @@ public class Main {
         int salaryVasiliy = 65_535;
         int moneyWallet = 15_000;
         int b = 0;
+        int bill1 = 12_000_000;
         double procentBank = (double) 7 / 100;
-        for (; moneyWallet < 12_000_000; b++) {
+        for (; moneyWallet < bill1; b++) {
             moneyWallet = moneyWallet + salaryVasiliy;
             moneyWallet = (int) (moneyWallet + (moneyWallet * procentBank));
             System.out.println(b + " mounth - " + moneyWallet + " rub");
@@ -75,28 +64,31 @@ public class Main {
         System.out.println("\nTask 2");
 
         int salaryVasiliy1 = 65_535;
-        int moneyWallet1 = 15_000;
+        int month = 15_000;
         int b1 = 0;
+        int bill2 = 12_000_000;
         double procentBank1 = (double) 7 / 100;
-        for (; moneyWallet1 < 12_000_000; b1++) {
-            moneyWallet1 = moneyWallet1 + salaryVasiliy1;
-            moneyWallet1 = (int) (moneyWallet1 + (moneyWallet1 * procentBank1));
+        for (; month < bill2; b1++) {
+            month = month + salaryVasiliy1;
+            month = (int) (month + (month * procentBank1));
             if (b1 % 6 == 0) {
-                System.out.println(b1 + " mounth - " + moneyWallet1 + " rub");
+                System.out.println(b1 + " mounth - " + month + " rub");
             }
         }
 
         System.out.println("\nTask 3");
 
         int salaryVasiliy3 = 65_535;
-        int moneyWallet3 = 15_000;
+        int month1 = 15_000;
         int b3 = 0;
+        int monthInYear = 12;
+        int saveMonth = monthInYear * 9;
         double procentBank3 = (double) 7 / 100;
-        for (; b3 <= 108; b3++) {
-            moneyWallet3 = moneyWallet3 + salaryVasiliy3;
-            moneyWallet3 = (int) (moneyWallet3 + (moneyWallet3 * procentBank3));
+        for (; b3 <= saveMonth; b3++) {
+            month1 = month1 + salaryVasiliy3;
+            month1 = (int) (month1 + (month1 * procentBank3));
             if (b3 % 6 == 0) {
-                System.out.println(b3 + " mounth - " + moneyWallet3 + " rub");
+                System.out.println(b3 + " mounth - " + month1 + " rub");
             }
         }
 
@@ -115,9 +107,12 @@ public class Main {
         System.out.println("\nHomework 3\nTask 1");
 
         int kometa = 79;
-        for (int y = 1822; y <= 2122; y++) {
-            if (y % kometa == 0) {
-                System.out.println(y + " year.");
+        int yearNow = 2022;
+        int yearBefore = yearNow - 200;
+        int yearAfter = yearNow + 100;
+        for (; yearBefore <= yearAfter; yearBefore++) {
+            if (yearBefore % kometa == 0  ) {
+                System.out.println(yearBefore + " year.");
             }
         }
 
